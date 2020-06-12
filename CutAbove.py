425560039df7126fd2f3e83d78fa32515ea07257
Matthew Owen <matthewsowen@gmail.com>
2020-06-11 22:52:09 -0400

    Code for Streamlit web app

--- CutAbove.py
+++ CutAbove.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from pyzipcode import ZipCodeDatabase
 import streamlit as st
 
@@ -11,73 +10,84 @@ import streamlit as st
 '''
 
 
-MA_Docs = pd.read_csv('MassDocs.csv')
+L_Docs = pd.read_csv('Lung_Surgeons.csv', index_col = 0)
 
-MA_Hosp = pd.read_csv('MassHosp.csv')
+L_Hosp = pd.read_csv('L_hosps.csv', index_col = 0)
 
 
-st.selectbox('What kind of surgery do you need?',
-             ('GI Surgery', 'Lung Surgery'))
 
-ZIP = st.text_input('Please enter your ZIP Code', 
+ZIP = st.text_input('Please enter your ZIP code', 
                     max_chars = 5)
 
-radius = st.number_input('Radius (in miles)')
+radius = st.number_input(label = 'Radius (in miles)',
+                         min_value = 1, 
+                         value = 25,
+                         step = 1)
 
 zipdb = ZipCodeDatabase()
 
-in_radius = [z.zip for z in zipdb.get_zipcodes_around_radius(
-        ZIP, radius)]
+if len(ZIP) == 5:
 
-HinZ = []
-
-for i in range(len(MA_Hosp['ZIP Code'])):
+    try:
+        in_radius = [z.zip for z in zipdb.get_zipcodes_around_radius(ZIP, radius)]
     
-    if MA_Hosp['ZIP Code'][i][0:5] in in_radius:
-        
-        HinZ.append(str(MA_Hosp.iloc[i, 2]))
-        
-    else:
-        
-        pass
-
-
-Local_Docs = pd.DataFrame(columns = ['Surgeons', 'Gender', 'Hospitals', 'Problem Rate'])
-
-for d in range(len(MA_Docs['Surgeons'])):
     
-    sp = MA_Docs.iloc[d, 3].split(';')
-    
-    for h in sp:
+        HinZ = []
             
-        if h in HinZ:
+        for i in range(len(L_Hosp['ZIP Code'])):
                 
-            df = MA_Docs.iloc[d, :]
-            Local_Docs = Local_Docs.append(df, ignore_index = True) 
-            break
-                
-        else:
-                
-            pass
+            if L_Hosp['ZIP Code'][i][0:5] in in_radius:
+                    
+                HinZ.append(str(L_Hosp.iloc[i, 0]))
+                    
+            else:
+                    
+                pass
+            
         
-Local_Docs.sort_values(by = ['Problem Rate'],
-                       inplace = True)
+        Local_Docs = pd.DataFrame(columns = ['Surgeons', 'Hospitals',
+                                                 'Problem Rate', 'Class',
+                                                 'Predicted Class', 'Rating'])
+            
+        for d in range(len(L_Docs['Surgeons'])):
+                
+            sp = L_Docs.iloc[d, 1].split(';')
+                
+            for h in sp:
+                        
+                if h in HinZ:
+                            
+                    df = L_Docs.iloc[d, :]
+                    Local_Docs = Local_Docs.append(df, ignore_index = True) 
+                    break
+                            
+                else:
+                            
+                    pass
+                    
+        Local_Docs.sort_values(by = ['Predicted Class'],
+                               inplace = True)
+            
+            
+            
+        '''
+        #### Here are top the surgeons in your area!
+        '''
+            
+        Local_Docs.set_index('Surgeons', 
+                             drop = True, inplace = True)
+            
+        Local_Docs2 = Local_Docs.replace(to_replace = 
+                                         {';[A-Za-z0-9-; ]+':''},
+                                         regex = True)
+            
+        Local_Docs3 = Local_Docs2.replace(to_replace = 
+                                          {';':''},
+                                          regex = True)
+            
+        st.table(Local_Docs3[['Hospitals', 'Rating']][0:30])
 
 
-
-'''
-#### Here are the top surgeons in your area!
-'''
-
-Local_Docs.set_index('Surgeons', 
-                     drop = True, inplace = True)
-
-Local_Docs2 = Local_Docs.replace(to_replace = 
-                                 {';[A-Za-z0-9-; ]+':''},
-                                 regex = True)
-
-Local_Docs3 = Local_Docs2.replace(to_replace = 
-                                 {';':''},
-                                 regex = True)
-
-st.table(Local_Docs3.iloc[0:10,0:2])
+    except:
+        
+        st.write('That doesn\'t appear to be a valid ZIP code!')
